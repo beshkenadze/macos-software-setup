009451f390ecaa4cb6a9bd0af4f404d0dfd69fcc
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2020-12-24 17:08:56 +0300

    Update personal-apps.sh

--- personal-apps.sh
+++ personal-apps.sh
@@ -1,2 +1,2 @@
 #!/bin/env sh
-brew install --cask 1password spotify istat-menus bartender beardedspice telegram-desktop 
+brew install --cask 1password spotify istat-menus bartender beardedspice telegram-desktop authy  
